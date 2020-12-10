139c182906b26dfb6c3b7848560e7bb308a2efb8
gxkjsfxy <74277437+gxkjsfxy@users.noreply.github.com>
2020-12-10 22:32:27 +0800

    Update 011.App.java与Zero.java移动矩阵.java

--- 011.App.java与Zero.java移动矩阵.java
+++ 011.App.java与Zero.java移动矩阵.java
@@ -8,42 +8,65 @@ public class App {
     JPanel myPanel=new JPanel();
     JLabel label_txt=new JLabel();
     JLabel label_foreground=new JLabel();
-    int map[][]={
-        {1,3,2},
-        {4,7,6},
-        {9,8,5},
-        {10,11,0}
+    int array2d[][]={
+            {1,3,2},
+            {4,7,6},
+            {9,8,5},
+            {10,11,0}
     };
     int cell=128;
     List<JLabel> label_list=new ArrayList<>();
-    //构造方法
+    //构造方法App()
     public App() {
-//网格
+        showMyGame();
+        myPanel.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                if(e.getKeyCode()==e.VK_UP){
+                    new Zero().goDown(array2d);
+                    showMyGame();
+                    new ShowArray2d(array2d);
+                }else if(e.getKeyCode()==e.VK_DOWN){
+                    new Zero().goUp(array2d);
+                    showMyGame();
+                    new ShowArray2d(array2d);
+                }else if(e.getKeyCode()==e.VK_LEFT){
+                    new Zero().goRight(array2d);
+                    showMyGame();
+                    new ShowArray2d(array2d);
+                }else if(e.getKeyCode()==e.VK_RIGHT){
+                    new Zero().goLeft(array2d);
+                    showMyGame();
+                    new ShowArray2d(array2d);
+                }else {}
+            }
+        });
+
+    }
+
+    //    为了使程序结构清晰，设置一个方法来显示所有组件showMyGame()
+    void showMyGame(){
+        //移动矩阵
+        for (int row = 0; row < array2d.length; row++) {
+            for (int column = 0; column < array2d[0].length; column++) {
+                label_list.add(new JLabel());
+                java.net.URL imgURL = App.class.getResource("img/nezha_"+String.valueOf(array2d[row][column])+".png");
+                label_list.get(row*3+column).setIcon(new ImageIcon(imgURL));
+                label_list.get(row*3+column).setBounds(column*cell,row*cell,cell,cell);
+                myPanel.add(label_list.get(row*3+column));
+                myPanel.repaint();
+            }
+        }
+    }
+
+    //显示窗体方法go()
+    void go(){
+        //网格
         java.net.URL imgURL999 = App.class.getResource("img/foreground.png");
         label_foreground.setIcon(new ImageIcon(imgURL999));
         label_foreground.setBounds(0,0,384,512);
         myPanel.add(label_foreground);
-
-        //移动矩阵
-        new Zero().goLeft(map);
-
-        for (int row = 0; row < map.length; row++) {
-            for (int column = 0; column < map[0].length; column++) {
-                label_list.add(new JLabel());
-                java.net.URL imgURL = App.class.getResource("img/nezha_"+String.valueOf(map[row][column])+".png");
-                label_list.get(row*3+column).setIcon(new ImageIcon(imgURL));
-                label_list.get(row*3+column).setBounds(column*cell,row*cell,cell,cell);
-                myPanel.add(label_list.get(row*3+column));
-            }
-        }
-
-
-
-    }
-    //显示窗体方法
-    void go(){
-
-
 
         label_txt.setText("这是一个Label标签。");
         label_txt.setBounds(0,0,200,30);
@@ -57,6 +80,7 @@ public class App {
         //frame.pack();
         frame.setVisible(true);
         frame.setResizable(false);//禁止用户修改大小
+        myPanel.setFocusable(true);//键盘监听必须加panel面板的焦点获取才可以
     }
     public static void main(String[] args) {
         new App().go();
